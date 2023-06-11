0ebe52869ec4affb51bc7d4e5ac1d97076aed180
SEYEON OH, VANESSA <102719063+SeoSeo@users.noreply.github.com>
2023-06-11 21:55:50 +0900

    flask 200 colorchange fin, controller ing

--- easyTerior_total/src/main/java/kr/spring/controller/StyleController.java
+++ easyTerior_total/src/main/java/kr/spring/controller/StyleController.java
@@ -133,6 +133,7 @@ public class StyleController {
 			// "http://127.0.0.1:5000/process_image" 에 newFilePath 경로 전달
 			// CSRF 토큰 가져오기
 			String csrfToken = multi.getParameter("_csrf");
+			session.setAttribute("csrfToken", csrfToken); // session에 저장
 			System.out.println("csrfToken : " + csrfToken);
 			
 			// 요청 헤더 설정
@@ -188,8 +189,8 @@ public class StyleController {
 				// 파일 이름 추출
 				String fileName = imgPath.substring(imgPath.lastIndexOf("/") + 1);
 				// 실질적인 프로젝트 이미지 경로
-				String realImagePath = "C:/eGovFrame-4.0.0/workspace.edu/.metadata/.plugins/org.eclipse.wst.server.core/tmp2/wtpwebapps/easyTerior_total/resources/images/flask/"
- + fileName;
+				// String realImagePath = "C:/eGovFrame-4.0.0/workspace.edu/.metadata/.plugins/org.eclipse.wst.server.core/tmp2/wtpwebapps/easyTerior_total/resources/images/flask/" + fileName;
+ 				String realImagePath = "C:/eGovFrame-4.0.0/workspace.edu/.metadata/.plugins/org.eclipse.wst.server.core/tmp0/wtpwebapps/easyTerior_total/resources/images/flask/" + fileName;
 				// 파일 이동
 				Path sourcePath = Paths.get(imgPath);
 				Path targetPath = Paths.get(realImagePath);
@@ -257,12 +258,12 @@ public class StyleController {
 	
 	@RequestMapping("/colorSelectForm.do")
 	public String colorSelectForm(@RequestParam("selectedObjectList") List<String> selectedObjectList, @RequestParam("selectedColorValue") String selectedColorValue, HttpSession session, RedirectAttributes rttr) {
-		System.out.println("colorSelectForm called");
+		System.out.println("\n\ncolorSelectForm called");
 		System.out.println("selectedObjectList : "+selectedObjectList);
 		System.out.println("selectedColorValue : "+selectedColorValue);
 		try {
 			// CSRF 토큰 가져오기
-			String csrfToken = (String) session.getAttribute("_csrf.token");
+			String csrfToken = (String) session.getAttribute("csrfToken");
 			System.out.println("csrfToken : " + csrfToken);
 
 		    // 사용자가 업로드한 이미지 경로 가져오기
@@ -273,33 +274,40 @@ public class StyleController {
 		    
 		    // RestTemplate를 통해 flask의 "http://127.0.0.1:5000/color_change"로 request를 보내기
 		    HttpHeaders headers = new HttpHeaders();
-		    headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+		    // headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
 
-		    MultiValueMap<String, String> body = new LinkedMultiValueMap<>();
-		    body.add("_csrf", csrfToken);
+		    // MultiValueMap<String, String> body = new LinkedMultiValueMap<>();
+		    MultiValueMap<String, Object> body = new LinkedMultiValueMap<>();
+		    body.add("csrfToken", csrfToken);
 	        body.add("originalImg", originalImg);
 	        body.addAll("selectedObjectList", selectedObjectList);
 	        body.add("selectedColorValue", selectedColorValue);
 	        body.add("imageDir", imageDir);
 
-		    HttpEntity<MultiValueMap<String, String>> requestEntity = new HttpEntity<>(body, headers);
+		    //HttpEntity<MultiValueMap<String, String>> requestEntity = new HttpEntity<>(body, headers);
+	        HttpEntity<MultiValueMap<String, Object>> requestEntity = new HttpEntity<>(body, headers);
+	        System.out.println("requestEntity : "+requestEntity);
 		    
-		    System.out.println("requestEntity : "+requestEntity);
-		    
+		    // RestTemplate 생성
 		    RestTemplate restTemplate = new RestTemplate();
+
 		    ResponseEntity<String> responseEntity = restTemplate.postForEntity(
-		            "http://127.0.0.1:5000/color_change",
-		            requestEntity,
-		            String.class
+		    	    "http://127.0.0.1:5000/color_change",
+		    	    requestEntity,
+		    	    String.class
 		    );
 		    
-		 // 4. response가 성공적이면 "colorChangeResult.do"로 redirect하기
+		    System.out.println("\n\nresponseEntity : "+responseEntity);
+		    
+		    // 4. response가 성공적이면 "colorChangeResult.do"로 redirect하기
 		    if (responseEntity.getStatusCode().is2xxSuccessful()) {
 		    	// 성공적인 응답 처리
 				String jsonResponse = responseEntity.getBody();
 				// URL 디코딩 및 파싱하여 detection_result와 img_path 값을 추출
-				String detectionResult = null;
-				String imgPath = null;
+				String img_data = null;
+				String naver_img = null;
+				String final_img = null;
+				String original_img = null;
 				try {
 					jsonResponse = URLDecoder.decode(jsonResponse, "UTF-8");
 					String[] queryParams = jsonResponse.split("\\?")[1].split("&");
@@ -309,22 +317,51 @@ public class StyleController {
 				            String key = keyValue[0];
 				            String value = keyValue[1];
 				            
-				            if (key.equals("detection_result")) {
-				                detectionResult = value;
-				            } else if (key.equals("img_path")) {
-				                imgPath = value;
+				            if (key.equals("img_data")) {
+				            	img_data = value;
+				            } else if (key.equals("image_urls")) {
+				            	naver_img = value;
+				            }else if (key.equals("final_img")) {
+				            	final_img = value;
+				            }else if (key.equals("original_img")) {
+				            	original_img = value;
 				            }
 				        }
 				    }
+					System.out.println("\n\nSuccessful jsonResponse");
+					System.out.println("img_data : "+img_data);
+					System.out.println("naver_img"+naver_img);
+					System.out.println("final_img"+final_img);
+					System.out.println("original_img"+original_img);
+					
+					// 파일을 이클립스에서 인식하도록 옮기기
+					// 파일 이름 추출
+					String fileName = final_img.substring(final_img.lastIndexOf("/") + 1);
+					// 실질적인 프로젝트 이미지 경로
+					// String realImagePath = "C:/eGovFrame-4.0.0/workspace.edu/.metadata/.plugins/org.eclipse.wst.server.core/tmp2/wtpwebapps/easyTerior_total/resources/images/flask/" + fileName;
+	 				String realImagePath = "C:/eGovFrame-4.0.0/workspace.edu/.metadata/.plugins/org.eclipse.wst.server.core/tmp0/wtpwebapps/easyTerior_total/resources/images/flask/" + fileName;
+					// 파일 이동
+					Path sourcePath = Paths.get(final_img);
+					Path targetPath = Paths.get(realImagePath);
+					Files.copy(sourcePath, targetPath, StandardCopyOption.REPLACE_EXISTING); // 복사해서 이동
 
+					
+					// 추출한 detection_result와 img_path 값을 세션에 저장
+					if (jsonResponse != null) {
+					    session.setAttribute("img_data", img_data); // 색깔 정보
+					    session.setAttribute("naver_img", naver_img); // 네이버 쇼핑 API 결과
+					    session.setAttribute("final_img", final_img); // flask에서 처리된 이미지
+					    session.setAttribute("original_img", original_img); // 사용자가 업로드 한 원본 이미지 경로
+					    
+					}
+					
 					
 			    	return "redirect:/colorChangeResult.do";
 				} catch (Exception e) {
-					// TODO Auto-generated catch block
 					System.out.println("colorSelectForm Exception called");
 					e.printStackTrace();
 					rttr.addFlashAttribute("msgType", "실패 메세지");
-		            rttr.addFlashAttribute("msg", "이미지 변화에 실패하였습니다. flask에서 문제가 있습니다."); 
+		            rttr.addFlashAttribute("msg", "이미지 변화에 실패하였습니다. jsonResponse 에서 문제가 있습니다."); 
 			        return "redirect:/colorSelect.do";
 				}
 		    } else {
--- flask/app.py
+++ flask/app.py
@@ -1,4 +1,4 @@
-#!/D:/total/flaskrun/Scripts/python.exe
+# #!/D:/total/flaskrun/Scripts/python.exe
 # 가상환경의 python을 통해 실행되도록 처리.
 # common
 import os
@@ -7,6 +7,7 @@ import json
 import requests
 import subprocess
 import urllib.parse
+import configparser
 
 # Data Manipulation
 import numpy as np
@@ -72,19 +73,13 @@ handler.setFormatter(formatter)
 app.logger.addHandler(handler)
 
 CORS(app)
-UPLOAD_FOLDER = 'D:/total/easyTerior_total/src/main/webapp/resources/images/flask/'
+UPLOAD_FOLDER = 'C:/total/easyTerior_total/src/main/webapp/resources/images/flask/'
+# UPLOAD_FOLDER = 'D:/total/easyTerior_total/src/main/webapp/resources/images/flask/'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-FLASK_IMAGE = 'D:/total/flask/images/'
+# FLASK_IMAGE = 'D:/total/flask/images/'
+FLASK_IMAGE = 'C:/total/flask/images/'
 app.config['FLASK_IMAGE'] = FLASK_IMAGE
 
-# 확장자 안 바꿀 때,
-def fileRename(oldfile, name):
-    original_filename = os.path.basename(oldfile)
-    filename, extension = os.path.splitext(original_filename)
-    print("original_filename : ", original_filename)
-    print("filename : ",filename," | extension : ", extension)
-    print("fileRename called Result : ", (filename + name + extension))
-    return filename + name + extension
 
 # 확장자 변경 시,
 def fileRename(oldfile, name, ext):
@@ -95,10 +90,58 @@ def fileRename(oldfile, name, ext):
     print("fileRename called Result : ", (filename + name + "." + ext))
     return filename + name + "."+ ext
 
+# 확장자 안 바꿀 때,
+def fileRenameNoExt(oldfile, name):
+    print('fileRenameNoExt')
+    original_filename = os.path.basename(oldfile)
+    filename, extension = os.path.splitext(original_filename)
+    print("original_filename : ", original_filename)
+    print("filename : ",filename," | extension : ", extension)
+    print("fileRename called Result : ", (filename + name + extension))
+    return filename + name + extension
+
+def get_object_label(object_id):
+    switcher = {
+        0: "침대",
+        1: "이불",
+        2: "카펫",
+        3: "의자",
+        4: "커튼",
+        5: "문",
+        6: "램프",
+        7: "베개",
+        8: "선반",
+        9: "소파",
+        10: "테이블"
+    }
+    return switcher.get(object_id, "알 수 없음")
+
+def closest_color(rgb):
+    colors = {"빨간색": [0, 0, 255], "주황색": [0, 165, 255], "노랑색": [0, 255, 255], 
+            "초록색": [0, 255, 0], "파란색": [255, 0, 0], "남색": [255, 0, 255], 
+            "보라색": [238, 130, 238], "흰색": [255, 255, 255], "검은색": [0, 0, 0]}
+
+    closest_name = None
+    closest_distance = None  # Use 'closest_distance' instead of 'closest_color'
+    for name, color in colors.items():
+        distance = np.linalg.norm(np.array(color) - np.array(rgb))
+        if closest_distance is None or distance < closest_distance:
+            closest_distance = distance
+            closest_name = name
+
+    return closest_name
+
 
 
 @app.route('/process_image', methods=['POST'])
 def process_image():
+    # 2023-06-11 20:54:40,152 - ERROR - Error color_change image: local variable 'request' referenced before assignment
+    # 2023-06-11 20:54:40,154 - ERROR - Traceback (most recent call last):
+    # File "C:\total\flask\app.py", line 295, in color_change
+    #     csrfToken = request.form.get('csrfToken')
+    # UnboundLocalError: local variable 'request' referenced before assignment
+    # 위 에러에 대해 아래에 새로 import 처리
+    from flask import request # request 객체를 참조하기 전에 request 변수를 선언하고 초기화
     try:
         # 요청에서 newFilePath와 csrfToken 가져오기
         origin_path = request.form.get('newFilePath')
@@ -112,7 +155,6 @@ def process_image():
         image = cv2.imread(image_path)
 
         # 객체 탐지 로직 구현
-        # TODO: 이미지 처리 및 객체 탐지 코드 작성
 
         # Read the image using OpenCV
         nparr = np.frombuffer(image_file.read(), np.uint8)
@@ -130,25 +172,29 @@ def process_image():
         cv2.imwrite(save_path, resized_image)
         print("save_path : ", save_path)
 
-        script = "D:/total/yolov5/segment/predict.py"
-        bestPt = "D:/total/yolov5/myBest.pt"
+        script = "C:/total/yolov5/segment/predict.py"
+        bestPt = "C:/total/yolov5/myBest.pt"
+        # script = "D:/total/yolov5/segment/predict.py"
+        # bestPt = "D:/total/yolov5/myBest.pt"
         args = ["--weights", bestPt, "--img", "736", "--conf", "0.2", "--source", save_path, "--retina-masks", "--save-txt"]
 
         # 가상환경의 파이썬 실행 파일 경로
-        python_executable = "D:/total/flaskrun/Scripts/python.exe"
+        # python_executable = "D:/total/flaskrun/Scripts/python.exe"
+        python_executable = "C:/venvs/flaskrun/Scripts/python.exe"
 
         # script_result  = 
         subprocess.run([python_executable, script] + args, capture_output=True, text=True)
         # print("script_result :",script_result)
 
-        folder_path = "D:/total/yolov5/runs/predict-seg/"  # 기존 폴더 경로
+        folder_path = "C:/total/yolov5/runs/predict-seg/"
+        # folder_path = "D:/total/yolov5/runs/predict-seg/"  # 기존 폴더 경로
         exp_prefix = "exp"
 
         # exp 폴더에서 가장 큰 숫자를 찾기
         exp_folders = [f for f in os.listdir(folder_path) if os.path.isdir(os.path.join(folder_path, f)) and f.startswith(exp_prefix)]
         latest_exp_folder = max(exp_folders, key=lambda x: int(x[len(exp_prefix):]) if x[len(exp_prefix):].isdigit() else -1)
         label_path = "labels/"+new_filename.split('.')[0]+".txt"
-
+        print(f'label_path : {label_path}')
 
         if latest_exp_folder:
             # 파일 경로 생성
@@ -159,7 +205,7 @@ def process_image():
             # Check if the file exists
             if not os.path.exists(file_path):
                 # 파일이 존재하지 않는 경우, JSP로 JSON 응답 반환
-                print('파일이 존재하지 않는 경우, JSP로 JSON 응답 반환')
+                print('file_path 파일이 존재하지 않는 경우, JSP로 JSON 응답 반환')
                 return redirect('http://localhost:8081/colorChange.do?message=' + urllib.parse.quote('Image not detected'))
 
             with open(file_path, 'r') as file:
@@ -223,7 +269,7 @@ def process_image():
 
         cv2.imwrite(UPLOAD_FOLDER+output_filename, output_image) # UPLOAD_FOLDER에 _processed 추가하여 생성.
         print('UPLOAD_FOLDER+output_filename :', UPLOAD_FOLDER+output_filename)
-
+        
         # Convert the list of detected object IDs to a JSON string
         detected_object_ids_json = json.dumps(detected_object_ids, ensure_ascii=False).encode('utf-8')
         # Redirect to the colorSelect.do URL
@@ -268,34 +314,52 @@ def process_image():
         print("Error processing image :", str(e))
         return jsonify({'error': 'Failed to process image'}), 500
 
+
 @app.route('/color_change', methods=['POST'])
 def color_change():
-    print('\n\ncolor_change called')
+    from flask import request
     try:
-         # 요청에서 가져오기
         csrfToken = request.form.get('csrfToken')
-        originalImg = request.form.get('originalImg') # 사용자가 업로드 한 이미지 경로로 가져온 이미지
-        selectedObjectList = request.form.get('selectedObjectList') # 사용자가 선택한 객체 목록
-        selectedColorValue = request.form.get('selectedColorValue') # 사용자가 선택한 색깔 r,g,b 값
-        imageDir = request.form.get('imageDir') # flask 에서 작업했던 exp 경로
-        print(f'csrfToken : {csrfToken}\noriginalImg : {originalImg} | {type(originalImg)}\nselectedObjectList : {selectedObjectList} | type : {type(selectedObjectList)}\nselectedColorValue : {selectedColorValue} | type : {type(selectedColorValue)}\nimageDir : {imageDir} | type : {type(imageDir)}\n')
-
+        originalImg = request.form.get('originalImg')  # 사용자가 업로드 한 이미지 경로로 가져온 이미지
+        selectedObjectList = request.form.getlist('selectedObjectList')  # 사용자가 선택한 객체 목록
+        selectedColorValue = request.form.get('selectedColorValue')  # 사용자가 선택한 색깔 r,g,b 값
         dict_rgb = json.loads(selectedColorValue)
+        imageDir = request.form.get('imageDir') # flask 에서 작업했던 exp 경로
 
-        folder_path = "D:/id57176422/yolov5/runs/predict-seg/"  # 기존 폴더 경로
+        # # 요청에서 가져오기
+        # csrfToken = request.form.get('csrfToken')
+        # originalImg = request.form.get('originalImg') # 사용자가 업로드 한 이미지 경로로 가져온 이미지
+        # selectedObjectList = request.form.getlist('selectedObjectList')  # 사용자가 선택한 객체 목록
+        # selectedColorValue = request.form.get('selectedColorValue') # 사용자가 선택한 색깔 r,g,b 값
+        # dict_rgb = json.loads(selectedColorValue)
+        # imageDir = request.form.get('imageDir') # flask 에서 작업했던 exp 경로
+        print(f'csrfToken : {csrfToken}\noriginalImg : {originalImg} | {type(originalImg)}\nselectedObjectList : {selectedObjectList} | type : {type(selectedObjectList)}\nselectedColorValue : {selectedColorValue} | type : {type(selectedColorValue)}\ndict_rgb : {dict_rgb} | type : {type(dict_rgb)}\nimageDir : {imageDir} | type : {type(imageDir)}\n')
+
+        r = dict_rgb['R']
+        g = dict_rgb['G']
+        b = dict_rgb['B']
+        bgr_color = (b, g, r)
+        print(f'bgr_color : {bgr_color} | type : {type(bgr_color)}\n')
+
+        folder_path = "C:/total/yolov5/runs/predict-seg/" 
+        # folder_path = "D:/total/yolov5/runs/predict-seg/"  # 기존 폴더 경로
         exp_prefix = "exp"
         # 이미지 읽기
-        label_path = os.path.join(imageDir, "labels/processed_image.txt")
+        label_dir = imageDir+"/labels/"
+        label_path = [filename for filename in os.listdir(label_dir) if filename.endswith('.txt')]
+        print(f'before label_path : {label_path}')
+        label_path = os.path.join(label_dir, label_path[0])
         label_path = label_path.replace("\\", "/")
+        # print(f'after label_path : {label_path}') # 좌표 다 나옴 확인
 
         with open(label_path, 'r') as file:
-                content = file.read().splitlines()  # Remove newline characters
-        # lines = content.split('\n')
+            content = file.read()# .splitlines()  # Remove newline characters
+        lines = content.split('\n')
         img_path = originalImg
         image = cv2.imread(img_path)
 
         coordinates_dict = {}
-        for line in content:
+        for line in lines:
             line_number, *coordinates = line.split(' ')
             if line_number.isdigit() and line_number in selectedObjectList:
                 if line_number in coordinates_dict:
@@ -355,12 +419,75 @@ def color_change():
                 #Hue(색조): 0에서 179까지의 범위를 갖습니다. 이 값은 색상의 종류를 나타냅니다. 예를 들어, 0은 빨간색에 해당하고, 60은 녹색에 해당합니다. 180은 다시 빨간색에 가까워지는 값입니다.
                 #Saturation(채도): 0에서 255까지의 범위를 갖습니다. 이 값은 색의 순수성 또는 탁하게 보이는 정도를 나타냅니다. 0은 무채색(흰색, 회색)에 해당하고, 255는 가장 높은 채도를 나타냅니다.
                 #Value(명도): 0에서 255까지의 범위를 갖습니다. 이 값은 색의 밝기를 나타냅니다. 0은 검은색에 해당하고, 255는 가장 밝은 색에 해당합니다.
+        final_img = fileRenameNoExt(img_path, "_final")
+        cv2.imwrite(final_img, image)
+        
+        bgr_color__json = json.dumps(bgr_color)
+        print(bgr_color__json)
+        
+        encoded_json = urllib.parse.quote(bgr_color__json)  # URL encoding
+        print(f'encoded_json : {encoded_json}')
+        
+        real_color=closest_color(bgr_color)
+        print(real_color)
+        # global selected_items_labels
+        print(selectedObjectList)
+        
+
+        # Naver 쇼핑 API 결과 적용
+        
+        # configparser 객체 생성
+        config = configparser.ConfigParser()
+
+        # config.ini 파일 읽기
+        config.read('config.ini')
+
+        # 값 가져오기
+        # client_id = config.get('Credentials', 'client_id')
+        # client_secret = config.get('Credentials', 'client_secret')
+        client_id = "YmwEYBDY7aXnrocNn5Zx"
+        client_secret = "0daNsnl46p"
+        print(f'client_id : {client_id} | client_secret : {client_secret}')
+        # 중복성을 제거합니다. 최대 크기 제한은 제거했습니다.
+        # selected_items_labels = list(set(selected_items_labels))
+        selected_items_labels = [get_object_label(int(ids)) for ids in selectedObjectList]
+        print(f'selected_items_labels : {selected_items_labels}')
+        all_image_urls = []
+
+        for label in selected_items_labels:
+            encText = urllib.parse.quote(label)
+            url = "https://openapi.naver.com/v1/search/image?query=" + encText
+            req = urllib.request.Request(url)
+            req.add_header("X-Naver-Client-Id",client_id)
+            req.add_header("X-Naver-Client-Secret",client_secret)
+            response = urllib.request.urlopen(req)
+            rescode = response.getcode()
+
+            if(rescode==200):
+                response_body = response.read()
+                response_dict = json.loads(response_body.decode('utf-8'))
+                # 리스트의 크기에 따라 이미지를 가져오는 개수를 조정합니다.
+                if len(selected_items_labels) <= 3:
+                    image_urls = ",".join([urllib.parse.quote(item['link']) for item in response_dict['items'][:2]]) if response_dict['items'] else ""
+                else:
+                    image_urls = urllib.parse.quote(response_dict['items'][0]['link']) if response_dict['items'] else ""
+                all_image_urls.append(image_urls)
+            else:
+                print("Error Code:" + rescode)
+        # 모든 이미지 URL을 쉼표로 구분하여 합칩니다.
+        all_image_urls_str = ",".join(all_image_urls)
+        
+        print(f'encoded_json : {encoded_json}\nall_image_urls_str : {all_image_urls_str}\n')
+        
+        redirect_url = "http://localhost:8081/colorChangeShowSave.do?img_data=" + encoded_json + "&image_urls=" + all_image_urls_str + "&final_img="+final_img + "&original_img=" + originalImg
+
+        return redirect_url # redirect(redirect_url)
+    
     except Exception as e:
         app.logger.error("Error color_change image: %s", str(e))
         app.logger.error(traceback.format_exc())  # Traceback 정보 기록
         print("Error color_change image :", str(e))
         return jsonify({'error': 'Failed to process image'}), 500
-
 
 
 if __name__ == '__main__':
